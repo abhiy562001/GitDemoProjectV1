f9ef810810ed1b5c4ec0dee131f7a9d3e66cd668
abhiy562001 <161271427+abhiy562001@users.noreply.github.com>
2025-03-26 00:20:29 +0530

    invalid login changes

--- src/test/java/TestCases/loginTest.java
+++ src/test/java/TestCases/loginTest.java
@@ -15,4 +15,12 @@ public class loginTest extends base {
 		loginP.login("Admin", "admin123");
 		System.out.println("user successfully logged in application with valid credential");
 	}
+	
+	@Test(description = "verifying login with Invalid credential")
+	public void verify_login_with_Invalid_credential() {
+		loginP = new loginPage(driver);
+		loginP.login("Admi", "admin123");
+		System.out.println("verified login with invalid credential");
+	}
+	
 }
